0fae50fd0de24c9c516215649a43f58cf0df755d
ShaikhImadoddin <shaikhimadoddin75@gmail.com>
2026-02-04 22:16:22 +0530

    Completed l3 of patterns

--- patternPrinting/L2PatternPrinting.java
+++ patternPrinting/L2PatternPrinting.java
@@ -91,6 +91,87 @@ public class L2PatternPrinting {
         }
     }
     
+    public static void rhombusPattern(int row, int col){
+        for(int i=0;i<row;i++){
+            // for spaces
+            for(int j=0;j<(row-i-1);j++){
+                System.out.print(" ");
+            }
+
+            // for stars
+            for(int j=0;j<row;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void hourGlassPattern(int row,int col){
+        // upper part
+        for(int i=0;i<row;i++){
+            // for spaces
+            for(int j=0;j<i;j++){
+                System.out.print(" ");
+            }
+
+            // for stars
+            for(int j=0;j<2*(row-i);j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // lower part
+        for(int i=0;i<row;i++){
+            // for spaces
+            for(int j=0;j<(row-i-1);j++){
+                System.out.print(" ");
+            }
+
+            // for stars
+            for(int j=0;j<2*(i + 1) ;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void zigZagPattern(int row,int col){
+        // upper part
+        for(int i=0;i<=row;i++){
+            // for spaces
+            for(int j=0;j<i;j++){
+                System.out.print(" ");
+            }
+
+            // for stars
+            for(int j=0;j<(2*(row-i)+1);j++){
+                if(j==0 || j==(2*(row-i)))
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+
+        // lower part
+        for(int i=0;i<row;i++){
+            // for spaces
+            for(int j=0;j<row-i-1;j++){
+                System.out.print(" ");
+            }
+            // for stars
+            for(int j=0;j<(2*i)+row;j++){
+                if(j==0 || j==(((2*i)+row)-1))
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
+
     public static void main(String[] args){
         Scanner sc =  new Scanner(System.in);
 
@@ -106,6 +187,11 @@ public class L2PatternPrinting {
         System.out.println();
         hollowDiamondPattern(row, col);
         System.out.println();
+        rhombusPattern(row, col);
+        System.out.println();
+        hourGlassPattern(row,col);
+        System.out.println();
+        zigZagPattern(row,col);
         sc.close();
     }
 }
