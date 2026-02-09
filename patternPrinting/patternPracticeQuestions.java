44bd1fb4cd7903725f934e72c26cb9ca284ddf08
ShaikhImadoddin <shaikhimadoddin75@gmail.com>
2026-02-09 22:26:05 +0530

    Solved Final 4 remaining pattern questions today now I am clear in for and nested loops

--- patternPrinting/patternPracticeQuestions.java
+++ patternPrinting/patternPracticeQuestions.java
@@ -66,6 +66,102 @@ public class patternPracticeQuestions {
         }
     }
 
+    public static void spiralMatrix(int row,int col){
+        int num = 1;
+        int top = 0, bottom = row - 1, left = 0, right = row - 1;
+        int[][] matrix = new int[row][row];
+
+        while(top <= bottom && left <= right){
+            for (int i = left; i <= right; i++) 
+                matrix[top][i] = num++;
+            top++;
+            for (int i = top; i <= bottom; i++) 
+                matrix[i][right] = num++;
+            right--;
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--)
+                     matrix[bottom][i] = num++;
+                bottom--;
+            }
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) 
+                    matrix[i][left] = num++;
+                left++;
+            }
+        }
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < row; j++) {
+                System.out.print(matrix[i][j]);
+                if (j < row - 1) System.out.print(" ");
+            }
+            System.out.println();
+        }
+
+    }
+
+        public static void printPattern(int N, char C) {
+        int mid = N / 2;
+
+        // Upper pyramid including middle line
+        for (int i = 0; i <= mid; i++) {
+            for (int j = 0; j < i; j++) 
+                System.out.print(" ");
+            for (int j = 0; j < N - 2*i; j++) 
+                System.out.print(C);
+            System.out.println();
+        }
+
+        // Lower inverted pyramid
+        for (int i = mid - 1; i >= 0; i--) {
+            for (int j = 0; j < i; j++) 
+                System.out.print(" ");
+            for (int j = 0; j < N - 2*i; j++) 
+                System.out.print(C);
+            System.out.println();
+        }
+    }
+
+    public static void fancyPattern2(int N) {
+        for (int i = 1; i <= N; i++) {
+            // Print leading spaces
+            for (int s = 0; s < N - i; s++) 
+                System.out.print(" ");
+
+            // Print increasing numbers
+            for (int j = 1; j <= i; j++) 
+                System.out.print(j);
+            // Print decreasing numbers
+            for (int j = i - 1; j >= 1; j--) 
+                System.out.print(j);
+            System.out.println();
+        }
+    }
+
+    public static void printHourglass(int N, char C) {
+        int mid = N / 2;
+
+        // Upper inverted pyramid
+        for (int i = 0; i <= mid; i++) {
+            for (int s = 0; s < i; s++) 
+                System.out.print(" ");
+            for (int j = 0; j < N - 2*i; j++) 
+                System.out.print(C);
+
+            System.out.println();
+        }
+
+        // Lower pyramid
+        for (int i = mid-1; i >= 0; i--) {
+            for (int s = 0; s < i; s++) 
+                System.out.print(" ");
+            for (int j = 0; j < N - 2*i; j++) 
+                System.out.print(C);
+
+            System.out.println();
+        }
+    }
+
     public static void main (String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the value of row: ");
@@ -78,6 +174,15 @@ public class patternPracticeQuestions {
         System.out.println();
         pascalTriangle(row,col);
         System.out.println();
+        spiralMatrix(row,col);
+        System.out.println();
+        // fancy pattern 1
+        printPattern(5, '$');
+        System.out.println();
+        fancyPattern2(row);
+        // fancy pattern 3
+        System.out.println();
+        printHourglass(5, '@');
         sc.close();
     }
 }
