9d4d655f80f96b04c888cadef79430229c9d342e
ShaikhImadoddin <shaikhimadoddin75@gmail.com>
2026-03-15 23:09:52 +0530

    Searching and sorting l2_binary_search square root find using binary search

--- Searching_And_Sorting/L2_Binary_Search.java
+++ Searching_And_Sorting/L2_Binary_Search.java
@@ -42,6 +42,32 @@ public class L2_Binary_Search {
         return peakIndex;
     }
 
+    public static int mySqrt(int x){
+        int s = 0;
+        int e = x;
+        int mid = 0;
+        long product = 0;
+        int ans = 0;
+        while(s <= e){
+
+            mid = s + (e-s)/2;
+
+            product = mid * mid;
+
+            if(product == x){
+                return mid;
+            }
+            else if(product > x){
+                e = mid - 1;
+            }
+            else if(product < x){
+                ans = mid;
+                s = mid + 1;
+            }
+        }
+        return ans;
+    }
+
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
@@ -59,10 +85,38 @@ public class L2_Binary_Search {
             Output:2
         */
 
-        ArrayList<Integer> nums = new ArrayList<>();
-        nums = takeInputInArrayList(sc);
-        int peakIndex = peakIndexInMountainArray(nums);
-        System.out.println("Peak index is: "+peakIndex);
+        // ArrayList<Integer> nums = new ArrayList<>();
+        // nums = takeInputInArrayList(sc);
+        // int peakIndex = peakIndexInMountainArray(nums);
+        // System.out.println("Peak index is: "+peakIndex);
+
+        // Find Square Root using Binary Search
+
+        /*
+        Example 1:
+
+        Input: x = 16
+
+        Output: 4
+
+        Explanation: The integer part of the square root of 16 is 4 since 4 * 4 = 16.
+
+        Example 2:
+        Input: x = 8
+
+        Output: 2
+
+        Explanation: The integer part of the square root of 8 is 2 since 3 * 3 = 9 (which is greater than 8), and 2 * 2 = 4, which is less than 8.
+        */
+
+        System.out.println("Enter the number: ");
+        int x = sc.nextInt();
+        int ans = mySqrt(x);
+        System.out.println("Sqaure root of "+x+" is :"+ans);
+
+        mySqrt(x);
+
+
 
     }
 }
