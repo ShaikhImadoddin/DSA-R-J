6aea204b140994ec103388eabf92dde1995c8b79
ShaikhImadoddin <shaikhimadoddin75@gmail.com>
2026-03-17 22:48:38 +0530

    Searching and Sorting L2 Binary Search Find pivot index in rotated array

--- Searching_And_Sorting/L2_Binary_Search.java
+++ Searching_And_Sorting/L2_Binary_Search.java
@@ -119,6 +119,40 @@ public class L2_Binary_Search {
         return false;
     }
 
+    public static int findPivotIndex(ArrayList<Integer> nums){
+        int n = nums.size();
+
+        int s = 0;
+        int e = n-1;
+
+        int mid = 0;
+
+        if (n==1){
+            return nums.get(0);
+        }
+
+        if(nums.get(0) < nums.get(n-1)){
+            return -1;
+        }
+
+        while(s <= e){
+            mid = s + (e-s)/2;
+
+           if(mid+1 < n && nums.get(mid) > nums.get(mid+1)){
+                return mid;
+           }
+           else if(nums.get(mid) < nums.get(0)){
+                e = mid - 1;
+           }
+           else if(nums.get(mid) >= nums.get(0)){
+                s = mid + 1;
+           }
+
+        }
+
+        return -1;
+    }
+
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
@@ -198,20 +232,30 @@ public class L2_Binary_Search {
         -109 <= target <= 109
         */
 
-        ArrayList<ArrayList<Integer>> matrix = new ArrayList<>();
-        matrix = takeInputInMatrix(sc);
+        // ArrayList<ArrayList<Integer>> matrix = new ArrayList<>();
+        // matrix = takeInputInMatrix(sc);
 
-        System.out.println("Enter the target: ");
-        int target = sc.nextInt();
-        boolean ans = searchInMatrix(matrix, target);
+        // System.out.println("Enter the target: ");
+        // int target = sc.nextInt();
+        // boolean ans = searchInMatrix(matrix, target);
 
 
-        if(ans == true){
-            System.out.println("Found");
-        }
-        else{
-            System.out.println("Not Found");
-        }
+        // if(ans == true){
+        //     System.out.println("Found");
+        // }
+        // else{
+        //     System.out.println("Not Found");
+        // }
+
+        // Find the pivot index in rotated array
+
+        ArrayList<Integer> nums = new ArrayList<>();
+        nums = takeInputInArrayList(sc);
+        int pivot = findPivotIndex(nums);
+        System.out.println("Pivot index is: "+pivot);
+
+
+
 
         sc.close();
     }
